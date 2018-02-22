056454f847dcc48cba6b80987177cb65ee8dc78d
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2018-02-22 23:33:43 +0800

    添加新的随机函数

--- HuaWeiCraft2017/random.cpp
+++ HuaWeiCraft2017/random.cpp
@@ -6,3 +6,7 @@ uint32_t Random::random_int(uint32_t min, uint32_t max) { // [min, max]
 double Random::random_double(double min, double max) { // [min, max)
 	return std::uniform_real_distribution<double>{min, max}(generator);
 }
+double Random::rnd(double dbLow, double dbUpper) {
+	double dbTemp = rand() / ((double)RAND_MAX + 1.0);
+	return dbLow + dbTemp*(dbUpper - dbLow);
+}
--- HuaWeiCraft2017/random.h
+++ HuaWeiCraft2017/random.h
@@ -9,6 +9,7 @@ public:
 	// Random(time_t t = time(NULL)): generator(t) {}
 	static inline uint32_t random_int(uint32_t min, uint32_t max);
 	static inline double random_double(double min, double max);
+	static double Random::rnd(double dbLow, double dbUpper)
 };
 
 #endif // ! RANDOM_H
